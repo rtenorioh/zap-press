0b80d19f9bd8b1499ff97194c53f93aea4ae70ee
Robson Tenorio <robson.tenorio@gmail.com>
2022-01-18 22:22:52 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,435 @@
+const { Client, List, Buttons, MessageMedia } = require('whatsapp-web.js');
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const socketIO = require('socket.io');
+const qrcode = require('qrcode');
+const http = require('http');
+const fs = require('fs');
+const { phoneNumberFormatter } = require('./helpers/formatter');
+const fileUpload = require('express-fileupload');
+const axios = require('axios');
+const mime = require('mime-types');
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(fileUpload({
+  debug: true
+}));
+
+const SESSION_FILE_PATH = './whatsapp-session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionCfg = require(SESSION_FILE_PATH);
+}
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: __dirname
+  });
+});
+
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
+  session: sessionCfg
+});
+
+client.on('message', async msg => {
+  //Setar as mensagens de BOT AQUI
+  if (msg.body == '!ping') {
+    msg.reply('pong');
+  } else if (msg.body == 'good morning') {
+    msg.reply('selamat pagi');
+  } else if (msg.body == '!groups') {
+    client.getChats().then(chats => {
+      const groups = chats.filter(chat => chat.isGroup);
+
+      if (groups.length == 0) {
+        msg.reply('Você ainda não tem um grupo.');
+      } else {
+        let replyMsg = '*SEUS GRUPOS*\n\n';
+        groups.forEach((group, i) => {
+          replyMsg += `ID: ${group.id._serialized}\nNome: ${group.name}\n\n`;
+        });
+        replyMsg += '_Você pode usar o ID do grupo para enviar uma mensagem ao grupo._'
+        msg.reply(replyMsg);
+      }
+    });
+  }
+
+});
+
+client.initialize();
+
+// Socket IO
+io.on('connection', function(socket) {
+  socket.emit('message', 'Connecting...');
+
+  client.on('qr', (qr) => {
+    console.log('QR RECEBIDO', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'QR Code recebido, escaneie por favor!');
+    });
+  });
+
+  client.on('ready', () => {
+    socket.emit('ready', 'Whatsapp está pronto!');
+    socket.emit('message', 'Whatsapp está pronto!');
+  });
+
+  client.on('authenticated', (session) => {
+    socket.emit('authenticated', 'Whatsapp é autenticado!');
+    socket.emit('message', 'Whatsapp é autenticado!');
+    console.log('AUTHENTICATED', session);
+    sessionCfg = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+
+  client.on('auth_failure', function(session) {
+    socket.emit('message', 'Falha de autenticação, reiniciando...');
+  });
+
+  client.on('disconnected', (reason) => {
+    socket.emit('message', 'Whatsapp está desconectado!');
+    fs.unlinkSync(SESSION_FILE_PATH, function(err) {
+        if(err) return console.log(err);
+        console.log('Arquivo de sessão excluído!');
+    });
+    client.destroy();
+    client.initialize();
+  });
+});
+
+
+const checkRegisteredNumber = async function(number) {
+  const isRegistered = await client.isRegisteredUser(number);
+  return isRegistered;
+}
+
+// Send message
+app.post('/send-message', [
+  body('number').notEmpty(),
+  body('message').notEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
+    });
+  }
+
+  const number = phoneNumberFormatter(req.body.number);
+  const message = req.body.message;
+
+  const isRegisteredNumber = await checkRegisteredNumber(number);
+
+  if (!isRegisteredNumber) {
+    return res.status(422).json({
+      status: false,
+      message: 'O número não está registrado'
+    });
+  }
+
+  client.sendMessage(number, message).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+});
+
+// Send media
+app.post('/send-media', async (req, res) => {
+  const number = phoneNumberFormatter(req.body.number);
+  const caption = req.body.caption;
+  const fileUrl = req.body.file;
+
+  // const media = MessageMedia.fromFilePath('./image-example.png');
+  // const file = req.files.file;
+  // const media = new MessageMedia(file.mimetype, file.data.toString('base64'), file.name);
+  let mimetype;
+  const attachment = await axios.get(fileUrl, {
+    responseType: 'arraybuffer'
+  }).then(response => {
+    mimetype = response.headers['content-type'];
+    return response.data.toString('base64');
+  });
+
+  const media = new MessageMedia(mimetype, attachment, 'Media');
+
+  client.sendMessage(number, media, {
+    caption: caption
+  }).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+});
+
+const findGroupByName = async function(name) {
+  const group = await client.getChats().then(chats => {
+    return chats.find(chat => 
+      chat.isGroup && chat.name.toLowerCase() == name.toLowerCase()
+    );
+  });
+  return group;
+}
+
+// Send message to group
+// You can use chatID or group name, yea!
+app.post('/send-group-message', [
+  body('id').custom((value, { req }) => {
+    if (!value && !req.body.name) {
+      throw new Error('Valor inválido, você pode usar `id` ou `name`');
+    }
+    return true;
+  }),
+  body('message').notEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
+    });
+  }
+
+  let chatId = req.body.id;
+  const groupName = req.body.name;
+  const message = req.body.message;
+
+  // Find the group by name
+  if (!chatId) {
+    const group = await findGroupByName(groupName);
+    if (!group) {
+      return res.status(422).json({
+        status: false,
+        message: 'Nenhum grupo encontrado com o nome de: ' + groupName
+      });
+    }
+    chatId = group.id._serialized;
+  }
+
+  client.sendMessage(chatId, message).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+});
+
+// Clearing message on spesific chat
+app.post('/clear-message', [
+  body('number').notEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
+    });
+  }
+
+  const number = phoneNumberFormatter(req.body.number);
+
+  const isRegisteredNumber = await checkRegisteredNumber(number);
+
+  if (!isRegisteredNumber) {
+    return res.status(422).json({
+      status: false,
+      message: 'O número não está registrado'
+    });
+  }
+
+  const chat = await client.getChatById(number);
+  
+  chat.clearMessages().then(status => {
+    res.status(200).json({
+      status: true,
+      response: status
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  })
+});
+
+// Send button
+app.post('/send-button', [
+  body('number').notEmpty(),
+  body('buttonBody').notEmpty(),
+  body('bt1').notEmpty(),
+  body('bt2').notEmpty(),
+  body('bt3').notEmpty(),
+  body('buttonTitle').notEmpty(),
+  body('buttonFooter').notEmpty()
+  
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
+    });
+  }
+
+  const number = phoneNumberFormatter(req.body.number);
+  const buttonBody = req.body.buttonBody;
+  const bt1 = req.body.bt1;
+  const bt2 = req.body.bt2;
+  const bt3 = req.body.bt3;
+  const buttonTitle = req.body.buttonTitle;
+  const buttonFooter = req.body.buttonFooter;
+  const button = new Buttons(buttonBody,[{body:bt1},{body:bt2},{body:bt3}],buttonTitle,buttonFooter);
+
+  const isRegisteredNumber = await checkRegisteredNumber(number);
+
+  if (!isRegisteredNumber) {
+    return res.status(422).json({
+      status: false,
+      message: 'O número não está registrado'
+    });
+  }
+
+  client.sendMessage(number, button).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+});
+
+
+app.post('/send-list', [
+  body('number').notEmpty(),
+  body('ListItem1').notEmpty(),
+  body('desc1').notEmpty(),
+  body('ListItem2').notEmpty(),
+  body('desc2').notEmpty(),
+  body('List_body').notEmpty(),
+  body('btnText').notEmpty(),
+  body('Title').notEmpty(),
+  body('footer').notEmpty()
+  
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
+    });
+  }
+
+  const number = phoneNumberFormatter(req.body.number);
+  const sectionTitle = req.body.sectionTitle;
+  const ListItem1 = req.body.ListItem1;
+  const desc1 = req.body.desc1;
+  const ListItem2 = req.body.ListItem2;
+  const desc2 = req.body.desc2;
+  const List_body = req.body.List_body;
+  const btnText = req.body.btnText;
+  const Title = req.body.Title;
+  const footer = req.body.footer;
+
+  const sections = [{title:sectionTitle,rows:[{title:ListItem1, description: desc1},{title:ListItem2, description: desc2}]}];
+  const list = new List(List_body,btnText,sections,Title,footer);
+
+  const isRegisteredNumber = await checkRegisteredNumber(number);
+
+  if (!isRegisteredNumber) {
+    return res.status(422).json({
+      status: false,
+      message: 'O número não está registrado'
+    });
+  }
+
+  client.sendMessage(number, list).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+});
+
+server.listen(port, function() {
+  console.log('Aplicativo em execução na porta: ' + port);
+});
---
+++ readme.md
@@ -0,0 +1,18 @@
+# ZAP-PRESS
+
+Hi, this is the implementation example of <a href="https://github.com/pedroslopez/whatsapp-web.js">whatsapp-web.js</a>
+
+Watch the tutorials:
+
+- <a href="https://www.youtube.com/channel/UCrPbAoQKz42Gm0mLdWatAEA">Zap das Galáxias</a>
+
+
+### How to use?
+
+- Clone or download this repo
+- Enter to the project directory
+- Run `npm install`
+- Run `npm start`
+- Open browser and go to address `http://localhost:8000`
+- Scan the QR Code
+- Enjoy! 
